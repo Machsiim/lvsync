019b6d0ef6c3df08ab6504199ce7a3a88f6baf8e
Maxim R. <machsimbs@gmail.com>
2026-04-19 01:28:54 +0200

    working api endpoint, ical caching, dynamic ical range pulling from cis

--- lvsync.Python/main.py
+++ lvsync.Python/main.py
@@ -2,9 +2,13 @@ from dotenv import load_dotenv
 from icalendar import Calendar
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from fastapi import FastAPI, Query
 import os
 import requests
+import uvicorn
 
+
+app = FastAPI()
 
 load_dotenv()
 
@@ -13,27 +17,53 @@ PASSWORD = os.getenv("PASSWORD")
 
 VIENNA = ZoneInfo("Europe/Vienna")
 
+def get_range() -> tuple[int, int]:
+    now = datetime.now(VIENNA)
+    year = now.year
+    if now.month >= 8:  # WS: August bis Februar
+        begin = datetime(year, 8, 1, tzinfo=VIENNA)
+        end = datetime(year + 1, 2, 28, tzinfo=VIENNA)
+    else:  # SS: Februar bis Juli
+        begin = datetime(year, 2, 1, tzinfo=VIENNA)
+        end = datetime(year, 7, 31, 23, 59, 59, tzinfo=VIENNA)
+    return int(begin.timestamp()), int(end.timestamp())
+
+def get_ical_cis():
+    begin, end = get_range()
+    response = requests.get(f'https://cis.technikum-wien.at/cis/private/lvplan/stpl_kalender.php?type=student&pers_uid=if25b115&begin={begin}&ende={end}&format=ical&version=2&target=ical', auth=(USERNAME, PASSWORD))
+    with open("cache.ics", "wb") as f:
+        f.write(response.content)
+
 def get_ical():
-    response = requests.get('https://cis.technikum-wien.at/cis/private/lvplan/stpl_kalender.php?type=student&pers_uid=if25b115&begin=1770159600&ende=1785535200&format=ical&version=2&target=ical', auth=(USERNAME, PASSWORD))
-    cal = Calendar.from_ical(response.content)
-    return cal
+    with open("cache.ics") as f:
+        return Calendar.from_ical(f.read())
+
+
 
 def get_json_events(from_ts: datetime, to_ts: datetime):
-    
     cal = get_ical()
-    
+    events = []
+
     for event in cal.walk("VEVENT"):
         start = event.get("DTSTART").dt
-
         if from_ts <= start < to_ts:
-            summary = str(event.get("SUMMARY"))
-            print(summary)
-            
+            events.append({
+                "summary": str(event.get("SUMMARY")),
+                "location": str(event.get("LOCATION", "")),
+                "start": start.isoformat(),
+                "end": event.get("DTEND").dt.isoformat(),
+            })
 
-from_ts = datetime(2026, 4, 20, 0, 0, tzinfo=VIENNA)
-to_ts = datetime(2026, 4, 26, 0, 0, tzinfo=VIENNA)
+    return events
 
-get_json_events(from_ts, to_ts)
+                
+
+@app.get("/events")
+def get_events(from_ts: int, to_ts: int):
+    global VIENNA
+    print(get_json_events(datetime.fromtimestamp(from_ts, tz=VIENNA), datetime.fromtimestamp(to_ts, tz=VIENNA)))
+    return get_json_events(datetime.fromtimestamp(from_ts, tz=VIENNA), datetime.fromtimestamp(to_ts, tz=VIENNA))
 
 
-
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=6000, log_level="info")
